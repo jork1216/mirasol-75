877e45d0ae7fe4be5836fcc99c2895225f8f4a2d
junkcarlo16 <ramosjohncarlo16@gmail.com>
2026-05-24 23:00:02 +0800

    changes

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -19,7 +19,7 @@ export default function TestPage() {
           alt="Lola cover"
           className="block -translate-y-[10%] -translate-x-[10%] scale-140 md:scale-120"
         />
-        <div className="absolute top-[9%] left-[7%] z-10 text-center">
+        <div className="absolute top-[10%] left-[10%] z-10 text-center">
           <p className="text-[8px] font-medium tracking-[0.32em] text-white [text-shadow:0_2px_4px_rgba(0,0,0,0.7)] md:text-[11px] lg:text-[18px]">
             PLEASE JOIN US FOR A
           </p>
@@ -31,15 +31,15 @@ export default function TestPage() {
             Celebration
           </p>
           <p
-            className={`${cormorant.className} mt-4 text-[48px] font-bold leading-[0.8] text-white [text-shadow:0_3px_5px_rgba(0,0,0,0.85)] md:mt-5 md:text-[68px] lg:mt-7 lg:text-[116px]`}
+            className={`${cormorant.className} mt-4 text-[64px] font-bold leading-[0.8] text-white [text-shadow:0_3px_5px_rgba(0,0,0,0.85)] md:mt-5 md:text-[88px] lg:mt-7 lg:text-[116px]`}
           >
             Mirasol
           </p>
           <p
-            className={`${cormorant.className} -mt-1 text-[48px] font-bold italic leading-none text-[#f2c84b] [text-shadow:0_3px_5px_rgba(0,0,0,0.75)] md:text-[64px] lg:text-[112px]`}
-          >
-            @ 75
-          </p>
+          className={`${cormorant.className} -mt-1 text-[48px] font-bold italic leading-none text-[#f2c84b] [text-shadow:0_3px_5px_rgba(0,0,0,0.75)] md:text-[64px] lg:text-[112px]`}
+        >
+          @ <span className="text-[74px] md:text-[106px] lg:text-[148px]">75</span>
+        </p>
         </div>
         <div className="absolute top-[200%] left-1/2 z-10 h-[190vw] w-[190vw] -translate-x-1/2 -translate-y-1/2 rounded-full border-6 border-yellow-400 bg-white" />
         <div className="absolute top-0 left-0 z-0 h-full w-[90vw] bg-gradient-to-r from-yellow-900/90 to-transparent" />
